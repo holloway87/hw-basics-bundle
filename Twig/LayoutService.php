521f34afb035b9d721828b521a8ddcf52d974305
Holloway <me@holloway-web.de>
2014-07-28 19:47:09 +0200

    Added license and composer.json

--- Twig/LayoutService.php
+++ Twig/LayoutService.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * Copyright © 2014 Thomas Rudolph <me@holloway-web.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the LICENSE file for more details.
+ */
 
 namespace Hw\BasicsBundle\Twig;
 
